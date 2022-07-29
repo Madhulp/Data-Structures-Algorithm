d4de4a51ed09aa8155c0a03d75c189adeee4ad2a
madhul patil <madhulpatil7@gmail.com>
2022-07-29 10:20:54 +0530

    DSA

---
+++ level 3/Alan & Christopher.js
@@ -0,0 +1,38 @@
+function alan(s){
+    let stack=[];
+    for(let i=0;i<s.length;i++){
+        if(s[i]=='#')
+            stack.pop();
+        else
+            stack.push(s[i]);
+    }
+    return stack.join("");
+}
+function runProgram(input) {
+    let newInput=input.trim().split("\n");
+    let t=Number(newInput[0]);
+    for(let i=1;i<=t;i++){
+        let s=newInput[i];
+        console.log(alan(s));
+    }
+  }
+ if (process.env.USER === "") {
+   runProgram(``);
+ } else {
+   process.stdin.resume();
+   process.stdin.setEncoding("ascii");
+   let read = "";
+   process.stdin.on("data", function (input) {
+     read += input;
+   });
+   process.stdin.on("end", function () {
+     read = read.replace(/\n$/, "");
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+   });
+   process.on("SIGINT", function () {
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+     process.exit(0);
+   });
+ }
---
+++ level 3/Smaller Neighbour Element.js
@@ -0,0 +1,42 @@
+function smallerNeighbours(arr){
+    let stack=[];
+    let ans=[];
+    for(let i=0;i<arr.length;i++){
+        let smallest=-1;
+        while(stack.length !=0){
+            if (arr[i]>stack[stack.length-1]){
+               smallest=stack[stack.length-1];
+               break;
+            }else
+                stack.pop();
+        }
+        ans.push(smallest);
+        stack.push(arr[i]);
+    }
+    return ans.join(" ");
+}
+function runProgram(input) {
+    let newInput=input.trim().split("\n");
+    let arr=newInput[1].split(" ").map(Number);
+    console.log(smallerNeighbours(arr));
+  }
+ if (process.env.USER === "") {
+   runProgram(``);
+ } else {
+   process.stdin.resume();
+   process.stdin.setEncoding("ascii");
+   let read = "";
+   process.stdin.on("data", function (input) {
+     read += input;
+   });
+   process.stdin.on("end", function () {
+     read = read.replace(/\n$/, "");
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+   });
+   process.on("SIGINT", function () {
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+     process.exit(0);
+   });
+ }
