4857ad0d790cbfb8146becb378178348974e01ba
madhul patil <madhulpatil7@gmail.com>
2022-08-10 17:05:42 +0530

    DSA

--- Count such pairs!.js
+++ Count such pairs!.js
@@ -1,17 +1,17 @@
-function countSuchPairs(N,K,A){
+function countSuchPairs(N, K, A) {
     //write code here
-    let count=0;
-    let m=[];
-    for(let i=0;i<N;i++){
-        if(A[i]==0){
+    let count = 0;
+    let m = [];
+    for (let i = 0; i < N; i++) {
+        if (A[i] == 0) {
             continue
-        }else{
+        } else {
             m.push(A[i])
         }
     }
-    for(let i=0;i<m.length;i++){
-        for(let j=(i+1);j<m.length;j++){
-            if(m[i]+m[j]==K){
+    for (let i = 0; i < m.length; i++) {
+        for (let j = (i + 1); j < m.length; j++) {
+            if (m[i] + m[j] == K) {
                 count++;
             }
         }
